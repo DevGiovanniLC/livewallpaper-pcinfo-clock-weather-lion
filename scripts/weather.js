076e6e97908db98a76509247048d1f1daf4b7932
Giovanni León Corujo <giovannilc.dev@gmail.com>
2024-03-05 20:25:19 +0000

    weather, clock and pc stadistics done

---
+++ scripts/weather.js
@@ -0,0 +1,34 @@
+
+const apiKey = "81b26d93f2437095ac74c34866748a88";
+const city = "Las Palmas de Gran Canaria"
+const lon=-15.439
+const lat = 28.1165
+const units = "metric"
+const language = "es"
+
+var weatherID = setInterval(getWeather, 30000);
+getWeather()
+
+function getWeather() {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}&lang=${language}`)
+		.then((response) => response.json())
+		.then((data) => {
+			const weatherInfo = document.getElementById("weather-info")
+			if (data.cod === 200) {
+				const temperature = data.main.temp
+				const description = data.weather[0].description
+				const icon = data.weather[0].icon
+
+				weatherInfo.innerHTML= `
+            <p>${temperature}°C</p>
+            <p>${description}</p>
+            <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather icon">
+            `
+			} else {
+				weatherInfo.innerHTML = `<p>Error: ${data.message}</p>`
+			}
+		})
+		.catch((error) => {
+			console.error("Error fetching weather data:", error);
+		});
+}
